9d06e18410e19c8249621ced03e8d59ac6d85315
christopher mccarthy <chris@christophers-MacBook-Pro-2.local>
2019-09-15 11:45:05 -0700

    Added css animations on scroll and fixed sizing for buttons on mobile view

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -15,6 +15,49 @@
 var big_image;
 
 $(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+              $(this).animate({'opacity':'1'},700);
+            }
+        });
+        $('.hideme1').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+              $(this).animate({'opacity':'1'},1000);
+            }
+        });
+        $('.hideme2').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+              $(this).animate({'opacity':'1'},2000);
+            }
+        });
+        $('.hideme3').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+              $(this).animate({'opacity':'1'},2500);
+            }
+        });
+    });
+});
+
+$(document).ready(function() {
   BrowserDetect.init();
 
   // Init Material scripts for buttons ripples, inputs animations etc, more info on the next link https://github.com/FezVrasta/bootstrap-material-design#materialjs
